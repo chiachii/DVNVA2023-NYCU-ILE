6cd336c77aa8bdcf396c1fe94bf656a12ba6b287
chiachii <chiachii.liang@gmail.com>
2023-10-14 00:25:40 +0800

    upload template of hw5

---
+++ hw5/README.md
@@ -0,0 +1,20 @@
+## hw5: Stacked Bar Charts
+
+### About
+Craft stacked bar charts to represent the World University Rankings using D3.
+
+**Data Source:** https://www.kaggle.com/datasets/ddosad/timesworlduniversityrankings2024
+
+### Requirements
+**Direction of Stack:** You can choose either a vertical or horizontal stacking style.
+
+**Data Representation:** Every stacked bar should represent the total score of a particular university. Within each bar, the scores for different criteria—namely teaching, research, citations, industry income, and international—should be clearly distinguishable.
+
+**Sorting Capability:** Your visualization should enable sorting of universities:
+
+* By overall scores.
+* By each individual criterion (teaching, research, citations, industry income, and international).
+* Both in ascending and descending orders.
+
+### References
+[1] [D3.js Graph Gallery - basic stacked barplot](https://d3-graph-gallery.com/graph/barplot_stacked_basicWide.html)
---
+++ hw5/src/311554012.js
@@ -0,0 +1,92 @@
+// Load the dataset
+d3.csv('../data/TIMES_WorldUniversityRankings_2024.csv').then(data => {
+    // console.log(data);
+    render(data);
+});
+
+// Build the Stacked Bar Charts
+// Define the SVG dimensions and margins 
+const margin = { top: 20, right: 20, bottom: 20, left: 20};
+const width = 1200 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+// Create the SVG container
+const svg = d3.select('#stacked-bar-chart')
+    .append('svg')
+    .attr('width', 1300)
+    .attr('height', 540)
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+// Render Function
+const render = data => {
+    // `groups`: the name of each school 
+    const groups = data.map(d => (d.name)).slice(0, 50);
+
+    // `subgroups`: the header we need in the csv file
+    const subgroups = data.columns.slice(2, 14).filter(item => !item.includes('rank'));
+
+    // TODO:
+    // Preprocessing: delete rows without the value of 'score_overall'
+    data.forEach(d => {
+        if (isNaN(d['rank']) && d['rank'].startsWith('=')) {
+            // d['rank'] = parse(d['rank'].substring(1));
+            d['rank'] = d['rank'].replace('=', '');
+        }
+    });
+    data = data.filter(d => !isNaN(d['rank'])).slice(0, 50);
+    // console.log(data);
+
+    // Add X axis
+    const xScale = d3.scaleBand()
+        .domain(Object.keys(groups).map(key => parseInt(key) + 1))
+        .range([0, width])
+        .padding([0.5]);
+
+    svg.append('g')
+        .attr('transform', `translate(10, ${height})`)
+        .call(d3.axisBottom(xScale))
+            .selectAll('text')
+            .style('font-size', 10)
+            .style('text-anchor', 'end')
+            .attr('dx', '-1em')
+            .attr('dy', '-0.6em')
+            .attr('transform', 'rotate(-90)');
+
+    // Add Y axis
+    const yScale = d3.scaleLinear()
+        .domain([0, 600])
+        .range([height, 0]);
+        
+    svg.append('g')
+        .attr('transform', `translate(10, 0)`)
+        .call(d3.axisLeft(yScale));
+    
+    // colorScale = one color per subgroup
+    const colorScale = d3.scaleOrdinal()
+        .domain(subgroups)
+        .range(['#9ac8eb', '#d9a7c7', '#81C784', '#f3c7d5', '#90A4AE', '#f9c995']);
+
+    // Stack the data? --> stack per subgroup
+    const stackedData = d3.stack()
+        .keys(subgroups)
+        // .keys([subgroups[0], subgroups[2]])
+        (data);
+    // console.log(stackedData)
+    
+    // Show the bars
+    svg.append('g')
+        .selectAll('g')
+        // Enter in the stack data = loop key per key = group per group
+        .data(stackedData)
+        .join('g')
+            .attr('fill', d => colorScale(d.key))
+            .selectAll('rect')
+            // enter a second time = loop subgroup per subgroup to add all rectangles
+            .data(d => d)
+            .join('rect')
+            .attr('x', (d, i) => xScale(i+1)+10)
+            .attr('y', d => yScale(d[1]))
+            .attr('height', d => yScale(d[0]) - yScale(d[1]))
+            .attr('width', xScale.bandwidth());
+}; 
